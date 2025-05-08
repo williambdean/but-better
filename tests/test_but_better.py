c5f9fa1bc4cc8dab8e96108be06efe6fc2aeedd2
Will Dean <57733339+williambdean@users.noreply.github.com>
2025-05-08 10:43:06 -0400

    Support YouTube URL (#6)
    * automatically handle urls
    
    * up the version

--- tests/test_but_better.py
+++ tests/test_but_better.py
@@ -1,6 +1,10 @@
 import pytest
 
-from but_better import parse_youtube_url
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from but_better import parse_youtube_url, is_url
 
 
 @pytest.mark.parametrize(
@@ -12,3 +16,26 @@ from but_better import parse_youtube_url
 )
 def test_parse_youtube_url(url, expected):
     assert parse_youtube_url(url) == expected
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "https://www.youtube.com/watch?v=abcdefg",
+        "http://www.youtube.com/watch?v=abcdefg",
+    ],
+)
+def test_is_url_matches(value):
+    assert is_url(value)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "not_a_url",
+        "abcdefg",
+        "<your-youtube-id>",
+    ],
+)
+def test_is_url_does_not_match(value):
+    assert not is_url(value)
